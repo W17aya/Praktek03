08cb781472d82665b40ce888cc7ec8a966486ec2
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-04-09 01:18:24 +0800

    Getter and Setter

--- src/praktek03/MobilListrik.java
+++ src/praktek03/MobilListrik.java
@@ -8,4 +8,30 @@ public class MobilListrik extends Mobil {
             void throttle(){
               System.out.println("Suingsuing");
             }
+
+    public String getJenisMobil() {
+        return JenisMobil;
+    }
+
+    public void setJenisMobil(String JenisMobil) {
+        this.JenisMobil = JenisMobil;
+    }
+
+    public String getBahanBakar() {
+        return BahanBakar;
+    }
+
+    public void setBahanBakar(String BahanBakar) {
+        this.BahanBakar = BahanBakar;
+    }
+
+    public String getHarga() {
+        return Harga;
+    }
+
+    public void setHarga(String Harga) {
+        this.Harga = Harga;
+    }
+            
+            
 }
