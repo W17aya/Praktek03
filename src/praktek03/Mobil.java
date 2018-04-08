b91febe0d8f4fd43ca3b69e4622fdebf464f6f91
Eko Wijaya <ekowijayawindows@gmail.com>
2018-04-09 00:26:42 +0800

    buat method cetakinfo()

--- src/praktek03/Mobil.java
+++ src/praktek03/Mobil.java
@@ -4,4 +4,11 @@ public class Mobil {
     String merk;
     String warna;
     double harga;
+    
+    void cetakinfo(){
+        System.out.println("Merk \t:  "+merk+"\n"+
+                "Warna \t: "+warna+"\n"+
+                "Harga \t: "+harga);
+        
+}
 }
