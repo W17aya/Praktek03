9c2edc3d82812ffe72dc9bd262a4ccd732a880e6
Eko Wijaya <ekowijayawindows@gmail.com>
2018-04-09 00:22:45 +0800

    Membuat atribut

---
+++ src/praktek03/Mobil.java
@@ -0,0 +1,7 @@
+package praktek03;
+
+public class Mobil {
+    String merk;
+    String warna;
+    double harga;
+}
---
+++ src/praktek03/Praktek03.java
@@ -0,0 +1,17 @@
+
+package praktek03;
+
+/**
+ *
+ * @author W17aya
+ */
+public class Praktek03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
