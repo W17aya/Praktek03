26296e396e2490809ac099707abd89c53aff06ab
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-04-09 01:17:21 +0800

    attribute dan method

--- src/praktek03/MobilListrik.java
+++ src/praktek03/MobilListrik.java
@@ -1,4 +1,11 @@
 package praktek03;
+
 public class MobilListrik extends Mobil {
-    
+    private String JenisMobil;
+    private String BahanBakar;
+    private String Harga;
+            
+            void throttle(){
+              System.out.println("Suingsuing");
+            }
 }
