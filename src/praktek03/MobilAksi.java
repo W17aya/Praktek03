516cc56e5e763ec3da17a0ea4a5ff5e25250d91f
Eko Wijaya <ekowijayawindows@gmail.com>
2018-04-09 00:46:40 +0800

    instance, buat atribut dan panggil method

--- src/praktek03/MobilAksi.java
+++ src/praktek03/MobilAksi.java
@@ -2,6 +2,11 @@ package praktek03;
 
 public class MobilAksi {
   public static void main(String[] args){
+      Mobil l = new Mobil();
+      l.merk  = "Lamborghini";
+      l.warna = "Merah";
+      l.harga = 60000;
       
+      l.cetakInfo();
   }  
 }
