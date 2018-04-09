3a153379453f0357d90d703b828b777a36f399da
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-04-09 12:26:47 +0800

    Getter and Setter

--- src/praktek03/MobilSport.java
+++ src/praktek03/MobilSport.java
@@ -7,4 +7,30 @@ public class MobilSport extends Mobil {
    void throttle(){
        System.out.println("Nguuuung");
    }
+
+    public String getJenisMobil() {
+        return JenisMobil;
+    }
+
+    public void setJenisMobil(String JenisMobil) {
+        this.JenisMobil = JenisMobil;
+    }
+
+    public String getBahanBakar() {
+        return BahanBakar;
+    }
+
+    public void setBahanBakar(String BahanBakar) {
+        this.BahanBakar = BahanBakar;
+    }
+
+    public String getHarga() {
+        return Harga;
+    }
+
+    public void setHarga(String Harga) {
+        this.Harga = Harga;
+    }
+   
+   
 }
