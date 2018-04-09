889cc9cfbe05731d0bd08d4ae6a2bd88da6a7805
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-04-09 12:45:10 +0800

    Getter and Setter

--- src/praktek03/MobilAPV.java
+++ src/praktek03/MobilAPV.java
@@ -2,8 +2,37 @@ package praktek03;
 
 public class MobilAPV extends Mobil {
         private String JenisMobil;
+        private String BahanBakar;
+        private String  Harga;
+   
     
 void throttle(){
 System.out.println("Brmrmrmrmrmr");
 }
+
+    public String getJenisMobil() {
+        return JenisMobil;
+    }
+
+    public void setJenisMobil(String JenisMobil) {
+        this.JenisMobil = JenisMobil;
+    }
+
+    public String getBahanBakar() {
+        return BahanBakar;
+    }
+
+    public void setBahanBakar(String BahanBakar) {
+        this.BahanBakar = BahanBakar;
+    }
+
+    public String getHarga() {
+        return Harga;
+    }
+
+    public void setHarga(String Harga) {
+        this.Harga = Harga;
+    }
+
+
 }
