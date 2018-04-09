ede2cded3b79bc2b8a22b7c7702a13f326a311f7
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-04-09 12:24:40 +0800

    attribute dan method

--- src/praktek03/MobilSport.java
+++ src/praktek03/MobilSport.java
@@ -1,4 +1,10 @@
 package praktek03;
 public class MobilSport extends Mobil {
-    
+   private String JenisMobil;
+   private String BahanBakar;
+   private String Harga;
+   
+   void throttle(){
+       System.out.println("Nguuuung");
+   }
 }
